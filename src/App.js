39cce2a0579a940daba86aeea8f3dbf487953d07
MirzaHamzaManzoor <hamzamanzoor8234@gmail.com>
2020-09-23 14:05:28 -0700

    anychange

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Dictaphone from './components/Dictaphone'
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
