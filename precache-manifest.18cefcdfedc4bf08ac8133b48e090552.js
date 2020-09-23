51deaa13a5eef73a1aedcb4bc546eded14924028
MirzaHamzaManzoor <hamzamanzoor8234@gmail.com>
2020-09-23 14:07:09 -0700

    Updates

---
+++ precache-manifest.18cefcdfedc4bf08ac8133b48e090552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdab0346274672a21bf10e597757f89",
+    "url": "/reactSpeech/index.html"
+  },
+  {
+    "revision": "e71edd126533e0a6005d",
+    "url": "/reactSpeech/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60f538037d6bad460e33",
+    "url": "/reactSpeech/static/js/2.aab85bfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactSpeech/static/js/2.aab85bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71edd126533e0a6005d",
+    "url": "/reactSpeech/static/js/main.79f8b25b.chunk.js"
+  },
+  {
+    "revision": "2b1f94f10d6aa8494d20",
+    "url": "/reactSpeech/static/js/runtime-main.c99efba3.js"
+  }
+]);
